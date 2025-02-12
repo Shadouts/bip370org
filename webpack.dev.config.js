c6acd6b66b22f6486335c6a5a0eee8f27aa172f5
Shadouts <shadouts@hotmail.com>
2025-02-11 19:43:22 -0900

    update build config

---
+++ webpack.dev.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  watch: true,
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: { "stream": require.resolve("stream-browserify")}
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './'),
+  },
+  plugins: [
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+    }),
+],
+};
